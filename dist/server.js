6f4e06bea7c9d67c1f8de8cf8111d4e55a6b1882
Douglas Morais <mr.douglasmorais23@gmail.com>
2021-07-04 11:20:05 -0300

    Cors allow all origins

--- dist/server.js
+++ dist/server.js
@@ -3,5 +3,5 @@ var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
 var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
 
 const PORT = process.env.PORT || 3000;
-_app2.default.use(_cors2.default)
+_app2.default.use(_cors2.default.call(void 0, ));
 _app2.default.listen(PORT);
--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ import cors from 'cors';
 import app from './app';
 
 const PORT = process.env.PORT || 3000;
-app.use(cors)
+app.use(cors());
 app.listen(PORT);
